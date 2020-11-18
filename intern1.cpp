80965486b5954b599fc5d8fb1777f57820475ee4
vidhanshah <71776915+vidhanshah@users.noreply.github.com>
2020-11-18 11:34:23 +0530

    Create intern1.cpp

---
+++ intern1.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+int main()
+{
+    for(ll i=1;i<=7;i++)
+    {
+        for(ll j=i;j<=7;j++)
+            cout<<j<<" ";
+    }
+
+for(ll i=6;i>=1;i--)
+{
+    for(ll j=i;j<=7;j++)
+        cout<<j<<" ";
+}
+}
